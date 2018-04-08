7667a89b432ce9c1e1a4a4de7082ccb0ec028690
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-04-08 11:51:55 +0200

    added stack

---
+++ inc/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by przem on 08.04.2018.
+//
+
+#ifndef SNAKE_STACK_H
+#define SNAKE_STACK_H
+
+#endif //SNAKE_STACK_H
---
+++ src/stack.c
@@ -0,0 +1,5 @@
+//
+// Created by przem on 08.04.2018.
+//
+
+#include "stack.h"
